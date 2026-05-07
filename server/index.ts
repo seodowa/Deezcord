36b3a0e4f2f0afd225e02a0cf9dfeb83208f7a1f
seodowa <pagalan.theodoreustp@gmail.com>
2026-05-07 22:25:41 +0800

    refactor: dynamic path name for client dist

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// Determine if we are running from the compiled 'dist' folder or the root 'server' folder
+const clientDistPath = __dirname.endsWith('dist') 
+  ? path.join(__dirname, '../../client/dist') 
+  : path.join(__dirname, '../client/dist');
+
+app.use(express.static(clientDistPath));
 
 // 2. Tell Express to use the routes we separated
 app.use('/api/auth', authRoutes);
@@ -33,7 +38,7 @@ app.get('/api', (req: Request, res: Response) => {
 });
 
 app.get('/{*splat}', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  res.sendFile(path.join(clientDistPath, 'index.html'));
 });
 
 const server = http.createServer(app);
